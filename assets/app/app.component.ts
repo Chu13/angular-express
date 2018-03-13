4601a68e6e4050b8e523fc55b57af6e683944e59
Jesus Bordones <jabordones92@gmail.com>
2018-03-12 22:23:19 -0400

    New components created

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+
 })
 export class AppComponent {
-    
+  message = {
+    content: "A message",
+    author: "Chu"
+  }
+
 }
