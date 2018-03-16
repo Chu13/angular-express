381d52e3fa21659fcb84f93448a62de64b25e818
Jesus Bordones <jabordones92@gmail.com>
2018-03-16 13:07:15 -0400

    adding routing

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -6,15 +6,23 @@ import { AppComponent } from "./app.component";
 import { MessageComponent } from "./messages/message.component";
 import { MessageListComponent } from "./messages/messages-list.component";
 import { MessageInputComponent } from "./messages/messages-input.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { AuthenticationComponent } from "./auth/athentication.component";
+import { HeaderComponent } from "./header.component";
+import { routing } from './app.routing';
+
 
 @NgModule({
     declarations: [
         AppComponent,
         MessageComponent,
         MessageListComponent,
-        MessageInputComponent
+        MessageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent,
+        HeaderComponent
     ],
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, routing],
     bootstrap: [AppComponent]
 })
 export class AppModule {
